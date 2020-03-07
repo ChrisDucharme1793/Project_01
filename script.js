531fefa6892adcfc384d4a7ad88a960dfd509d84
kevin cox <kevincox1983@gmail.com>
2020-03-07 10:29:28 -0600

    particles

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
 $(document).ready(function() {
-  
   //adding event listeners
   $("#findBtn").on("click", citySearch);
   $("#searchForm").on("submit", citySearch); // Enables pressing enter to search
-
   
-
   function citySearch(e) {
     e.preventDefault();
-
     if ($("#cityInput").val() !== "") {
       //only run event if search form isn't empty
       $("#cardDiv").hide();
-
       let city = $("#cityInput").val();
-
-      const queryURL = `https://api.foursquare.com/v2/venues/explore?client_id=PGRFNMWYIQORRWIDI35ABOQZAFAB1AGPMHSLQ4DVNNA3FXQ5&client_secret=BFXFR3RHV1PI232XQ3A2FZ45L2K0LB22ZWWLHBLC5OJ3FX1P&v=20180323&limit=10&near=${city}&query=brewery`;
+      const queryURL = `https://api.foursquare.com/v2/venues/explore?client_id=PGRFNMWYIQORRWIDI35ABOQZAFAB1AGPMHSLQ4DVNNA3FXQ5&client_secret=BFXFR3RHV1PI232XQ3A2FZ45L2K0LB22ZWWLHBLC5OJ3FX1P&v=20180323&limit=10&near=${city}&query=food_trucks`;
       $.ajax({
         url: queryURL,
         method: "GET"
@@ -27,15 +21,12 @@ $(document).ready(function() {
           },
           500
         );
-
         //these are coords for creating map
         let lat = response.response.geocode.center.lat;
         let long = response.response.geocode.center.lng;
          console.log(lat);
         console.log(long);
-
         //explore endpoint gets a list of recommended breweries in the search area
-
         let venueArray = response.response.groups[0].items;
      let featuresArray = []; //array for map icons
         //add info of each brewery to features array so we can use them to display the icons on the map
@@ -81,20 +72,16 @@ $(document).ready(function() {
           }
         });
       });
-
          // Create a popup, but don't add it to the map yet.
   var popup = new mapboxgl.Popup({
     closeButton: false,
     closeOnClick: true
   });
-
   map.on("mouseenter", "places", function(e) {
     // Change the cursor style as a UI indicator.
     map.getCanvas().style.cursor = "pointer";
-
     var coordinates = e.features[0].geometry.coordinates.slice();
     var description = e.features[0].properties.description;
-
     // Populate the popup and set its coordinates
     // based on the feature found.
     popup
@@ -102,14 +89,7 @@ $(document).ready(function() {
       .setHTML(description)
       .addTo(map);
   });
-
-        
-
-       
-        
       });
     }
   }
 });
-
-
